b61744a29b48be8bef78d4058ed13fdefd97a8a9
Alaa_Omran <99070234+lolooppo@users.noreply.github.com>
2024-02-28 02:03:10 +0200

    Update HW3.cpp

--- Hashing/HW/HW3.cpp
+++ Hashing/HW/HW3.cpp
@@ -1,103 +1,40 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<algorithm>
 using namespace std;
 
-/***********************************************************/
-/*                 Author : Alaa Omran                     */
-/*                 Object : Binary Trees                   */
-/***********************************************************/
-
-// tree node
-struct node{
-    int data;
-    node *left{nullptr} , *right{nullptr};
-    node(int data):data(data){}
-};
-
-//tree class
-class binary_search_tree{
-private:
-    node* root{nullptr};
-
-    //print sorted
-    void print(node* temp){
-        if(temp->left)
-            print(temp->left);
-        cout<<temp->data<<" ";
-        if(temp->right)
-            print(temp->right);
-    }
-
-    //add to the tree
-    void add(int data,node* temp){
-        if(data<temp->data){
-            if(temp->left){
-                add(data,temp->left);
-            }else{
-                temp->left = new node(data);
-            }
-        }else{
-            if(temp->right){
-                add(data,temp->right);
-            }else{
-                temp->right = new node(data);
-            }
-        }
-    }
-
-    //clear
-    void clear(node *temp){
-        if(!temp)
-            return;
-        clear(temp->left);
-        clear(temp->right);
-        delete temp;
-    }
-
-public:
-    //constructor
-    binary_search_tree(int data):root(new node(data)){}
-
-    //print sorted
-    void print(){
-        node* temp{root};
-        print(temp);
-        cout<<"\n";
-    }
-
-    //add to the tree
-    void add(int data){
-        node* temp{root};
-        add(data,temp);
-    }
-
-    
-    /*******************
-        Home work
-    *******************/
-    
-    //check if the given preorder is degenerate tree or not
-    bool  degenerate(vector<int>& v){
-        int n = v.size();
-        if(!n)
-            return false;
-        if(n == 1 or n == 2)
-            return true;
-        for(int i = 2;i<n;i++){
-            int c = v[i] , b = v[i-1] , a = v[i-2];
-            if(b>a and c>a and (c<b or c>b)){
-                 continue;
-            }else if(b<a and c<a and (c<b or c>b)){
-                continue;
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //destructor
-    ~binary_search_tree(){
-        node* temp{root};
-        clear(temp);
+// This class will help unordered_set get hash for the object
+// To properly understand, you need to study operator overloading
+struct VectorHash {
+    size_t operator()(const std::vector<int>& v) const {
+        hash<int> hasher;
+        size_t seed = 0;
+        for (int i : v)
+            seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        return seed;
     }
 };
+
+
+int count_anagram_substrings(const string &str) {	// O(L^3)
+	unordered_set<vector<int>, VectorHash> s1;
+	for (int start = 0; start < (int) str.size(); ++start) {
+		for (int end = start; end < (int) str.size(); ++end) {
+			vector<int> freq(26);	// O(const)
+			for (int i = start; i <= end; ++i)
+				++freq[str[i] - 'a'];
+			s1.insert(freq);
+		}
+	}
+	return s1.size();
+}
+
+int main() {
+	cout << count_anagram_substrings("abba") << "\n";		// 6
+	cout << count_anagram_substrings("aaaaa") << "\n";		// 5
+	cout << count_anagram_substrings("abcba") << "\n";		// 9
+	cout << count_anagram_substrings("aabade") << "\n";		// 17
+
+	return 0;
+}
